4a8ec141818c50fa4c7700432628317194633b19
Sam Bahrami <bahrami.sam@gmail.com>
2025-09-23 14:21:47 +1000

    script

---
+++ assets/js/custom-script.js
@@ -0,0 +1,8 @@
+// GoatCounter Analytics
+(function() {
+    var script = document.createElement('script');
+    script.setAttribute('data-goatcounter', 'https://samb.goatcounter.com/count');
+    script.async = true;
+    script.src = '//gc.zgo.at/count.js';
+    document.head.appendChild(script);
+})();
